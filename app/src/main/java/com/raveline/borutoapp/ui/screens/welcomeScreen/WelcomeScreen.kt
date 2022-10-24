6e3ea9d6fef41262af5e75d7e177269c817992d6
ravelinejunior <junior.raveline@gmail.com>
2022-10-24 21:56:06 +0100

    Welcome Screen Detailed

--- app/src/main/java/com/raveline/borutoapp/ui/screens/welcomeScreen/WelcomeScreen.kt
+++ app/src/main/java/com/raveline/borutoapp/ui/screens/welcomeScreen/WelcomeScreen.kt
@@ -1,20 +1,26 @@
 package com.raveline.borutoapp.ui.screens.welcomeScreen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.*
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.pager.*
 import com.raveline.borutoapp.data.model.OnBoardingPage
 import com.raveline.borutoapp.ui.theme.*
 
@@ -36,11 +42,30 @@ fun WelcomeScree(navController: NavController) {
             .background(color = MaterialTheme.colors.welcomeScreenBackgroundColor)
     ) {
         HorizontalPager(
+            modifier = Modifier.weight(10f),
             count = pages.size,
             state = pagerState,
             verticalAlignment = Alignment.Top
         ) { page ->
             PagerScreen(onBoardingPage = pages[page])
+        }
+
+        // Dot Indicator
+        HorizontalPagerIndicator(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterHorizontally),
+
+            pagerState = pagerState,
+            activeColor = MaterialTheme.colors.activeColorIndicator,
+            inactiveColor = MaterialTheme.colors.inactiveColorIndicator,
+            indicatorWidth = MEDIUM_PADDING,
+            spacing = SMALL_PADDING
+        )
+
+        //Finish Button
+        FinishButton(pagerState = pagerState, modifier = Modifier.weight(1f)) {
+
         }
     }
 
@@ -100,5 +125,39 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
         )
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState: PagerState,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(
+            horizontal = LARGE_PADDING
+        ), verticalAlignment = Alignment.Top, horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(
+            modifier = Modifier
+                .padding(horizontal = EXTRA_LARGE_PADDING)
+                .fillMaxWidth(),
+            visible = pagerState.currentPage == 2
+        ) {
+            Button(
+                onClick = onClick, colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Blue80,
+                    contentColor = White,
+                ),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = "Finish", style = TextStyle(
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                ))
+            }
+        }
     }
 }
--- app/src/main/java/com/raveline/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/raveline/borutoapp/ui/theme/Color.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
@@ -44,3 +45,21 @@ val Colors.titleWelcomeColor
 val Colors.descriptionWelcomeColor
     @Composable
     get() = if (isLight) DarkGray.copy(alpha = 0.5f) else LightGray.copy(alpha = 0.5f)
+
+val Colors.activeColorIndicator
+    @Composable
+    get() = if (isLight) Blue60 else Blue40
+
+val Colors.inactiveColorIndicator
+    @Composable
+    get() = if (isLight) LightGray else DarkGray
+
+
+
+
+
+
+
+
+
+
