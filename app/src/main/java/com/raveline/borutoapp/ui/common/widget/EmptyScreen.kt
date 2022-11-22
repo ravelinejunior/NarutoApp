d3ff66b01a648560a29e09ff0acda759550e5695
ravelinejunior <franklin.junior@ctw.bmwgroup.com>
2022-11-22 16:15:39 +0000

    Implement Swipe to Refresh

--- app/src/main/java/com/raveline/borutoapp/ui/common/widget/EmptyScreen.kt
+++ app/src/main/java/com/raveline/borutoapp/ui/common/widget/EmptyScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,8 +17,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
 import com.airbnb.lottie.compose.*
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.raveline.borutoapp.R
+import com.raveline.borutoapp.data.model.HeroModel
 import com.raveline.borutoapp.ui.theme.DarkGray
 import com.raveline.borutoapp.ui.theme.LightGray
 import com.raveline.borutoapp.ui.theme.SMALL_PADDING
@@ -25,7 +31,8 @@ import java.net.SocketTimeoutException
 
 @Composable
 fun EmptyScreen(
-    error: LoadState.Error?=null
+    error: LoadState.Error? = null,
+    heroes: LazyPagingItems<HeroModel>? = null
 ) {
     var message by remember {
         mutableStateOf("Find your hero!")
@@ -35,7 +42,7 @@ fun EmptyScreen(
         mutableStateOf(R.raw.searching)
     }
 
-    if(error != null){
+    if (error != null) {
         message = parseErrorMessage(error)
         resRaw = R.raw.internet_error
     }
@@ -53,49 +60,77 @@ fun EmptyScreen(
         startAnimation = true
     }
 
-    EmptyContent(alphaAnim = alphaAnim, message = message, resRaw = resRaw)
+    EmptyContent(
+        alphaAnim = alphaAnim,
+        message = message,
+        resRaw = resRaw,
+        heroes = heroes,
+        error = error
+    )
 
 }
 
 
 @Composable
-fun EmptyContent(alphaAnim: Float, message: String, resRaw: Int) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
+fun EmptyContent(
+    alphaAnim: Float,
+    message: String,
+    resRaw: Int,
+    heroes: LazyPagingItems<HeroModel>? = null,
+    error: LoadState.Error? = null,
+) {
 
-        val composition: LottieCompositionResult = rememberLottieComposition(
-            spec = LottieCompositionSpec.RawRes(resRaw)
-        )
 
-        val progress by animateLottieCompositionAsState(
-            composition = composition.value,
-            isPlaying = true,
-            speed = 1f,
-            iterations = LottieConstants.IterateForever,
-        )
-
-        LottieAnimation(
+    var isRefreshing by remember {
+        mutableStateOf(false)
+    }
+    SwipeRefresh(
+        swipeEnabled = error != null,
+        state = rememberSwipeRefreshState(isRefreshing = isRefreshing),
+        onRefresh = {
+            isRefreshing = !isRefreshing
+            heroes?.refresh()
+            isRefreshing = !isRefreshing
+        }) {
+        Column(
             modifier = Modifier
-                .fillMaxHeight(0.68f)
-                .fillMaxWidth(0.9f),
-            composition = composition.value,
-            progress = progress,
-            maintainOriginalImageBounds = true
-        )
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
 
-        Text(
-            modifier = Modifier
-                .padding(top = SMALL_PADDING)
-                .alpha(alphaAnim),
-            text = message,
-            color = if (isSystemInDarkTheme()) LightGray else DarkGray,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            fontSize = MaterialTheme.typography.subtitle1.fontSize
-        )
+            val composition: LottieCompositionResult = rememberLottieComposition(
+                spec = LottieCompositionSpec.RawRes(resRaw)
+            )
+
+            val progress by animateLottieCompositionAsState(
+                composition = composition.value,
+                isPlaying = true,
+                speed = 1f,
+                iterations = LottieConstants.IterateForever,
+            )
+
+            LottieAnimation(
+                modifier = Modifier
+                    .fillMaxHeight(0.68f)
+                    .fillMaxWidth(0.9f),
+                composition = composition.value,
+                progress = progress,
+                maintainOriginalImageBounds = true
+            )
+
+            Text(
+                modifier = Modifier
+                    .padding(top = SMALL_PADDING)
+                    .alpha(alphaAnim),
+                text = message,
+                color = if (isSystemInDarkTheme()) LightGray else DarkGray,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            )
+        }
     }
 }
 
